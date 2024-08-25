05ce08c2a07ca0e1b52c15ee30c665f70a004b64
Kuldeep tak <Kuldeeptak2211@gmail.com>
2024-08-25 17:27:56 +0530

    project initialization

---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServicesPage = () => {
+  return <div className="text-white">ServicesPage</div>;
+};
+
+export default ServicesPage;
---
+++ src/pages/WorksPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WorksPage = () => {
+  return <div className="text-white">WorksPage</div>;
+};
+
+export default WorksPage;
